0f1cf64061b6d7cfa2416d1ddac6393420ed3216
Peter <peter.weir@gmail.com>
2017-02-11 18:25:50 -0800

    added PlayazClub

---
+++ PlayazClub/get_data.py
@@ -0,0 +1,112 @@
+from mapbox import Distance
+from mapbox import Geocoder
+
+geocoder = Geocoder(access_token=<ENTERYOURTOKENHERE>)
+distance_service = Distance(access_token=<ENTERYOURTOKENHERE>)
+
+place_names = ['1529 Fillmore St, San Francisco, CA 94115',
+'Sunnydale Ave, San Francisco, CA 94134',
+'242 Lakeview Ave, San Francisco, CA 94112',
+'Potrero Hill, San Francisco, CA',
+'Cesar Chavez St, San Francisco, CA',
+'Arizona',
+'Chicago, IL',
+'Philadelphia, PA',
+'Atlanta, GA',
+'Detroit, MI',
+'New York, NY',
+'Texas',
+'Seattle, WA',
+'Los Angeles, CA',
+'Bakersfield, CA',
+'San Diego, CA',
+'Palo Alto, CA',
+'Vallejo, CA',
+'Richmond, CA',
+'Sacramento, CA',
+'Oakland, CA',
+'3 Newcomb Ave, San Francisco, CA 94124']
+
+
+quote_names = ["Fillmoe (Fillmore)",
+"Sunnydale",
+"Lakeview",
+"P.H. (Potrero Hill)",
+"Army Street (now Cesar Chavez)",
+"Arizona",
+"Chicago",
+"Philly (Philadelphia)",
+"Atlanta",
+"Detroit",
+"New York",
+"Texas",
+"Seattle",
+"L.A.",
+"Bakersfield",
+"San Diego",
+"P.A. (Palo Alto)",
+"V-Town (Vallejo)",
+"Richmond",
+"Sacramento",
+"Biggity Biggity O (Oakland)",
+"H.P. (Hunters Point)"]
+
+quotes = ["Where your Playaz Club at 4? My Playaz Club right in the heart of Fillmoe",
+"Fillmoe, H.P. and Sunnydale, there's a playaz club everywhere you dwell",
+"Lakeview, P.H. and Army Street, a different part of town, A different kind of freak",
+"Lakeview, P.H. and Army Street, a different part of town, A different kind of freak",
+"Lakeview, P.H. and Army Street, a different part of town, A different kind of freak",
+"I'd rather kick it with the crew in Arizona, They chop game like we do in California",
+"I know they got a Playaz Club out there in Chicago",
+"What about that one they got out there in Philly, Fo?",
+"You know they got one out there in Atlanta, the way they by choppin",
+"Shit, Detroit, New York, Texas",
+"Shit, Detroit, New York, Texas",
+"Shit, Detroit, New York, Texas",
+"Yeah, but we gonna move on down to these Playaz Clubs close to home like Seattle, L.A., Bakersfield, San Diego",
+"Yeah, but we gonna move on down to these Playaz Clubs close to home like Seattle, L.A., Bakersfield, San Diego",
+"Yeah, but we gonna move on down to these Playaz Clubs close to home like Seattle, L.A., Bakersfield, San Diego",
+"Yeah, but we gonna move on down to these Playaz Clubs close to home like Seattle, L.A., Bakersfield, San Diego",
+"P.A., V-Town, Richmond, Sacramento",
+"P.A., V-Town, Richmond, Sacramento",
+"P.A., V-Town, Richmond, Sacramento",
+"P.A., V-Town, Richmond, Sacramento",
+"Yeah, but a special shot goes out to the Playaz Club right across the water in the Biggity Biggity O",
+"Uh, I feel you boy, where yours at Fly? Man, on the corner of Third and Newcomb right in the heart of H.P."]
+
+no_geocode = {}
+
+city_features = []
+for place_ind, place_name in enumerate(place_names):
+    quote_name = quote_names[place_ind]
+    quote = quotes[place_ind]
+    print place_ind + 1, 'of', len(place_names), place_name
+
+    response = geocoder.forward(place_name, country=['us'], limit=1)
+        
+    [lng, lat] = response.geojson()['features'][0]['center']
+    name = response.geojson()['features'][0]['place_name']
+    if response.geojson()['features'][0].has_key('bbox'):
+        bbox = response.geojson()['features'][0]['bbox']
+    else:
+        print 'no bounding box'
+        bbox = [lng-.001, lat-.001, lng+.001, lat+.001]
+        
+    city_feature = {'type': 'Feature', 'properties': {'quote': quote, 'bbox':bbox, 'quote_name':quote_name}, 'geometry': {'type': 'Point', 'coordinates': [lng, lat]}}
+    city_features.append(city_feature)
+
+response = distance_service.distances(city_features, 'driving')
+
+durations = response.json()['durations']
+
+out_file = open('durations.tsp', 'w')
+out_file.write('NAME: playersClub\nTYPE: TSP\nCOMMENT: drivingDurations\nDIMENSION: '+str(len(durations))+'\n')
+out_file.write('EDGE_WEIGHT_TYPE: EXPLICIT\nEDGE_WEIGHT_FORMAT: FULL_MATRIX\nEDGE_WEIGHT_SECTION\n')
+for durs in durations:
+    for d in durs:
+        out_file.write(str(int(round(d))) + ' ')
+    out_file.write('\n')
+
+out_file.close()
+
+
---
+++ PlayazClub/make_map.py
@@ -0,0 +1,153 @@
+#before running this script:
+#copy 'durations.tsp' to concorde/TSP directory,
+#then run './concorde -o out_durations.sol durations.tsp',
+#then copy 'out_durations.sol' to this directory.
+
+in_file = open('out_durations.sol', 'r')
+raw_in = in_file.readlines()
+ind_order = []
+for line in raw_in[1:]:
+    for ind in line.split():
+        ind_order.append(int(ind))
+        
+in_file.close()
+
+for city_ind in range(len(city_features)):
+    zoom =  max(15./(1 + city_features[city_ind]['properties']['bbox'][2] - city_features[city_ind]['properties']['bbox'][0]), 7)
+    city_features[city_ind]['properties']['zoom'] = zoom
+
+for _i, ind in enumerate(ind_order): 
+    if ind > 0:
+        duration_this_leg = durations[ind_order[_i-1]][ind]
+        total_duration += duration_this_leg
+    else:
+        duration_this_leg = 0
+        total_duration = 0
+    if duration_this_leg > 60*60:
+        dur_str = str(round(duration_this_leg/(60.*60.),1))+' hr'
+    else:
+        dur_str = str(round(duration_this_leg/(60.), 1))+' min'
+
+    print '<tr><td>'+city_features[ind]['properties']['quote_name']+'</td><td>'+dur_str+'</td></tr>'
+
+print '<tr><td>Total:</td><td>'+str(round(total_duration/(60.*60.),1))+' hr'+'</td></tr>'
+######################save map:
+locations_string = ''
+for ind in ind_order:
+    city_feature = city_features[ind]
+    locations_string = locations_string + """
+    {
+        "id": " """ + str(ind) +""" ",
+        "title": " """ + city_feature['properties']['quote_name'] + """ ",
+        "description": "'""" + city_feature['properties']['quote'].encode('utf-8') + """'",
+        "camera": {
+            center: [""" + str(city_feature['geometry']['coordinates'][0]) + ', ' + str(city_feature['geometry']['coordinates'][1]) + """],
+            zoom: """ + str(city_feature['properties']['zoom']) + """,
+            pitch: 50,
+            speed: 0.5
+        }
+    },"""
+
+locations_string = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <title></title>
+    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.31.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.31.0/mapbox-gl.css' rel='stylesheet' />
+    <style>
+        body { margin:0; padding:0; }
+        #map { position:absolute; top:0; bottom:0; width:100%; }
+    </style>
+</head>
+<body>
+
+<style>
+.map-overlay-container {
+    position: absolute;
+    width: 25%;
+    top: 0;
+    left: 0;
+    padding: 10px;
+    z-index: 1;
+}
+
+.map-overlay {
+    font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+    background-color: #fff;
+    border-radius: 3px;
+    padding: 10px;
+    box-shadow:0 1px 2px rgba(0,0,0,0.20);
+}
+
+.map-overlay h2,
+.map-overlay p {
+    margin: 0 0 10px;
+}
+</style>
+
+<div id='map'></div>
+
+<div class='map-overlay-container'>
+  <div class='map-overlay'>
+    <h2 id='location-title'></h2>
+    <p id='location-description'></p>
+    <small>Text credit: <a target='_blank' href='https://play.google.com/music/preview/Tr5rb4wvhquxz2fa2be56wntsm4?lyrics=1&utm_source=google&utm_medium=search&utm_campaign=lyrics&pcampaignid=kp-lyrics'>Google Play</a></small>
+  </div>
+</div>
+
+<script>
+mapboxgl.accessToken = 'pk.eyJ1IjoicHR3IiwiYSI6ImNpeHNodXh6dzAwMGMyd29jc3o0N3l2am4ifQ.IUyNISmPOGZJNdxuiyKrmA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-100, 40],
+    maxZoom: 15,
+    minZoom: 3,
+    zoom: 4
+});
+
+var title = document.getElementById('location-title');
+var description = document.getElementById('location-description');
+
+var locations = [""" + locations_string + """];
+
+
+function playback(index) {
+    title.textContent = locations[index].title;
+    description.textContent = locations[index].description;
+
+    // Animate the map position based on camera properties
+    map.flyTo(locations[index].camera);
+
+    map.once('moveend', function() {
+        // Duration the slide is on screen after interaction
+        window.setTimeout(function() {
+            // Increment index
+            index = (index + 1 === locations.length) ? 0 : index + 1;
+            playback(index);
+        }, 3000); // After callback, show the location for 3 seconds.
+    });
+}
+
+// Display the last title/description first
+title.textContent = locations[locations.length - 1].title;
+description.textContent = locations[locations.length - 1].description;
+
+map.on('load', function() {
+
+    // Start the playback animation for each borough
+    playback(0);
+});
+</script>
+
+</body>
+</html>
+
+"""
+out_file = open('map.html', 'w')
+out_file.write(locations_string)
+out_file.close()
+
